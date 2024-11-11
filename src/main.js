a92d48ca9fdabdc14ad011344162bbcfef916f1c
Arjun Krishnan <akrish29@ucsc.edu>
2024-11-11 15:41:33 -0800

    done

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 const usernamefield = document.getElementById('username');
 const passwordfield = document.getElementById('password');
 const loginForm = document.getElementById('loginForm');
+const answerQR = document.getElementById('answer');
 
 const correctUsername = 'cyrusXsilver._.vibes_03';
 const correctPassword = 'stoneflowerhelmet13';
@@ -15,7 +16,7 @@ loginForm.addEventListener('submit', function (event) {
   
     if (username === correctUsername && password === correctPassword) {
       // Redirect to another page if login is successful
-      window.location.href = 'https://ajnkrishnan.me'; // Replace with your desired URL
+      answerQR.style.display = 'block';
     } else {
       // Show an alert if login fails
       alert('Incorrect username or password');
